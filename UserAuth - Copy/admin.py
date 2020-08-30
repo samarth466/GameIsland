5da4e4d0769555a543d8414b3087f58af0b67542
Samarth Saxena <samarths466@gmail.com>
2020-08-30 00:18:01 -0400

    c

--- UserAuth - Copy/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from UserAuth.models import User
-
-# Register your models here.
-user_admin = admin.sites.AdminSite(name='User')
-user_admin.register(model_or_iterable=User)
--- UserAuth - Copy/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from UserAuth.forms import RegistrationForm
-
-class UserAuthTest(TestCase):
-    def test_registration_form_email(self):
-        form = RegistrationForm(data={"email": ''})
-        self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors["email"], ["This field is required."])
