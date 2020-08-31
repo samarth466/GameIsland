de436ee64ce009ddcbec4bb52f441a55c9d7486f
Samarth Saxena <samarths466@gmail.com>
2020-08-31 16:21:26 -0400

    c

--- UserAuth - Copy/validators.py
+++
@@ -1,7 +0,0 @@
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
-
-def validate_isnumeric(value):
-    value = str(value)
-    if not value.isnumeric():
-        raise ValidationError(_('%(value)s is an invalid pin'),params={'value': value})
--- practice_game - Copy/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PracticeGameConfig(AppConfig):
-    name = 'practice_game'
