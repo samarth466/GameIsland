7c681894a792de7061f3c9d098dc450455b98bf3
Samarth Saxena <samarths466@gmail.com>
2020-12-20 09:43:24 -0500

    added a few minor changes to Square class.

--- game/GamingScripts/board_utils/square.py
+++ game/GamingScripts/board_utils/square.py
@@ -1,16 +1,17 @@
 import pygame
-import chess_py
 
-class Square:
+class Square(object):
 
-    def __init__(self,rank,file,color,piece,square_length,is_empty=False):
+    def __init__(self,rank,file,color,piece,square_width,square_height,is_empty=False):
+        pygame.init()
         self.rank = rank
         self.file = file
         self.color = color
         self.is_empty = is_empty
         self.piece = piece
-        self.square_length = square_length
-        self.is_emptiable = True
+        self.square_width = square_width
+        self.square_height = square_height
+        self.is_attacked = False
     
     def get_window_pos(self):
         self.x = (self.rank-1)*100
@@ -20,10 +21,7 @@ class Square:
     
     def draw(self,win):
         x,y = self.get_window_pos()
-        pygame.init()
-        self.rect = pygame.Rect(x,y,self.square_length,self.square_length)
+        self.rect = pygame.Rect(x,y,self.square_width,self.square_height)
         self.draw_square = pygame.gfxdraw.rectangle(win,self.rect,self.color)
+        self.piece.draw(self.draw_square)
         return self.draw_square
-    
-    def is_under_attack(self):
-        
